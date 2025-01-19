c0c81506d73a8e90bf9e6f9a3e06e5b5160302fb
Leonardo-de-Moura <francisco.tico.fssm@gmail.com>
2025-01-19 19:13:54 -0300

    tasks feature

--- script/script.js
+++ script/script.js
@@ -1,5 +1,3 @@
-
-
 const modal = document.querySelector(".modal");
 console.log(modal);
 const fecharmodal = document.querySelector(".fecharmodal");
@@ -7,15 +5,28 @@ console.log(fecharmodal);
 const button = document.querySelector(".button");
 console.log(button);
 
+
 button.addEventListener("click", () => {
   modal.style.display = "flex";
 });
+
 
 fecharmodal.addEventListener("click", () => {
   modal.style.display = "none";
 });
 
+
+const tasks = [];
+
+
 function addtask() {
   let input = document.querySelector("#input").value;
-  
+  let time= document.querySelector(".hour").value;
+  if (input && time){
+    const objeto= {task:String(input), hour: time}
+
+    tasks.push(objeto)
+
+    console.log(JSON.stringify(tasks, null, 2))
+  }
 }
