e49c9da2d327e33865ee5bad78d7987026e6ace1
Leonardo-de-Moura <francisco.tico.fssm@gmail.com>
2025-02-07 10:55:48 -0300

    add task

--- script/script.js
+++ script/script.js
@@ -1,32 +1,29 @@
 const modal = document.querySelector(".modal");
-console.log(modal);
 const fecharmodal = document.querySelector(".fecharmodal");
-console.log(fecharmodal);
 const button = document.querySelector(".button");
-console.log(button);
-
 
 button.addEventListener("click", () => {
   modal.style.display = "flex";
 });
 
-
 fecharmodal.addEventListener("click", () => {
   modal.style.display = "none";
 });
 
-
-const tasks = [];
-
-
 function addtask() {
   let input = document.querySelector("#input").value;
-  let time= document.querySelector(".hour").value;
-  if (input && time){
-    const objeto= {task:String(input), hour: time};
-
-    tasks.push(objeto);
-
-    console.log(JSON.stringify(tasks, null, 2));
+  let time = document.querySelector(".hour").value;
+  if (input && time) {
+    const objeto = { task: input, hour: time };
+    console.log(JSON.stringify(objeto));
+    let task = (document.querySelector(
+      ".container"
+    ).innerHTML += `<div class="tasks">
+        <img src="assets/rec.png" alt="" />
+        <p>${objeto.task}</p>
+        <span>${objeto.hour}</span>
+      </div>`);
+      
   }
+  
 }
