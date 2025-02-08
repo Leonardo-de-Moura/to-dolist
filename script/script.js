10c163ecf3f7b33dada49f6af1bc110efe3e0b72
Leonardo-de-Moura <francisco.tico.fssm@gmail.com>
2025-02-08 14:47:45 -0300

    finish features

--- script/script.js
+++ script/script.js
@@ -6,33 +6,33 @@ function closemodal() {
   const modal = document.querySelector(".modal");
   modal.style.display = "none";
 }
-function marked() {
-  document.querySelector(".tasks").classList.add("marked");
-  document.querySelector(".img").src = "assets/check-mark.png";
-}
+
 function addtask() {
   let input = document.querySelector("#input").value;
   let time = document.querySelector(".hour").value;
+
   if (input && time) {
     const objeto = { task: input, hour: time };
     let task = (document.querySelector(
       ".container"
-    ).innerHTML += `<div onclick="marked()" class="tasks">
-        <img class="img" src="assets/rec.png" alt="" />
-        <p>${objeto.task}</p>
-        <span>${objeto.hour}</span>
-         <button onclick="deletetask()">
-          <img src="assets/trash.png" alt="">
+    ).innerHTML += `  <div class="tasks" onclick="marked(this)">
+        <img class="img" src="assets/rec.png" />
+        <p>${input}</p>
+        <span>${time}</span>
+        <button onclick="this.parentElement.remove()">
+          <img src="assets/trash.png" />
         </button>
       </div>`);
+
   }
 }
-function dates(){
-  let nowdate= new Date()
-  const date= document.querySelector(".time").innerText=nowdate;
+function marked(task) {
+  let img = task.querySelector(".img");
+  task.classList.toggle("marked");
+  img.src = task.classList.contains("marked") ? "assets/check-mark.png" : "assets/rec.png";
+}
+function dates() {
+  let nowdate = new Date();
+  const date = (document.querySelector(".time").innerText = nowdate);
 }
 dates();
-function deletetask(){
-  let task = document.querySelector(".tasks");
-  task.remove()
-}
