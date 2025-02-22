7a0e6980a58be45523dde207437f5fd071c7c16e
Leonardo-de-Moura <francisco.tico.fssm@gmail.com>
2025-02-22 19:57:24 -0300

    up

--- script/script.js
+++ script/script.js
@@ -1,6 +1,19 @@
-const button = document.querySelector(".button");
-const modal = document.querySelector(".modal");
+const Button = document.querySelector(".button");
+const Modal = document.querySelector(".modal");
+const CloseModal = document.querySelector(".close"); const inputTask = document.querySelector(".inputTask");const inputHour = document.querySelector(".inputHour");
 
-button.addEventListener("click", () => {
-	modal.style.display = "flex";
+Button.addEventListener("click", () => {
+	Modal.style.display = "flex";
 })
+
+CloseModal.addEventListener("click", () => {
+	Modal.style.display = "none";
+})
+const tasks= [1,2,3,4,5]
+
+console.log(tasks)
+
+
+
+
+//lista sera o database do projeto, ela recebera uma sequencia de objetos contendo as tasks que serao exibidos no DOM.
